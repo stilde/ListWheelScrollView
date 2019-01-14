f60cfd7b86baa395a82868c2103a2be658df487a
sylvain <sylvain@stilde.fr>
2019-01-14 15:46:04 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:list_wheel_scroll_view/widget.dart';
+
+void main() => runApp(MaterialApp(
+      home: S02E02(),
+      debugShowCheckedModeBanner: false,
+    ));
